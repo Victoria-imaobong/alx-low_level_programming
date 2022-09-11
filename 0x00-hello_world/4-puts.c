35256123bf69bf692e78a65792c49ba74e100ce0
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-11 11:53:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - print the sring in the put function
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
-
