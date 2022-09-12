73e6825069bc25a93bc80f5da6c96563dfa6a940
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-12 10:44:31 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++);
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}	
