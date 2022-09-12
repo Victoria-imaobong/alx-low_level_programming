f498f911979230316bb89b8f9822aec2493c7c7f
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-12 12:42:19 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int f, r, y;
+
+	for (f = '0'; f<= '9'; f++)
+	{
+	for (r = f + 1; r <= '9'; r++)
+	{
+	for (y = r + 1; y <= '9'; y++)
+	{
+	if ((r != f) != y)
+	{
+	putchar(f);
+	putchar(r);
+	putchar(y);
+	if (f == '7' && r == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
