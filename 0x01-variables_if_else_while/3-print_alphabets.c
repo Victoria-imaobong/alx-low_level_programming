45387101d9ac9b209194241ff5fa0a326f6b4919
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-12 01:32:12 -0700

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet in lowercase,then in uppercase.
 (*
@@ -19,6 +20,8 @@ int main(void)
 		putchar(d);
 		d++
 	}
-	putchar('\n');
+
+	putchar(10);
+
 	return (0);
 }	
