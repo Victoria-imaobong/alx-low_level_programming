39c1dab96d4880ec9bf6bf413773dc2b5ecc1b30
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-13 00:29:29 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int f, r;
+
+	for (f = 0; f <= 98; f++)
+	{
+	for (r = f + 1; r <= 99; r++)
+	{
+	putchar((f / 10) + '0');
+	putchar((f % 10) + '0');
+	putchar(' ');
+	putchar((r / 10) + '0');
+	putchar((r % 10) + '0');
+	if (f == 98 && r == 99)
+	continue;
+	putchar(',');
+	putchar(',');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
