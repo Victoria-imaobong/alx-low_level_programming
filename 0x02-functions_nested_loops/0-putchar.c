58fb1ac690c97c98f5ef5d582b1dcf56ad122c1c
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-13 11:06:04 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
+
 {
-	printf("_putchar\n");
+	int a;
+
+	while (a)
+	printf("_putchar");
+
 	return (0);
 }
