d05a6d4c2fb7a4d3e0826ae823f8216f65a37ecd
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-17 06:27:23 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * main - tests that the function positive_or_negative() gives the correct output when given a case of 0.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
