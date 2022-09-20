4086475ad4fa98de8bfeea539343b1f5e87933f0
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-20 01:11:18 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi -a function that converts a string to an integer
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+
+	return (0);
+}
