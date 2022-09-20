8191fe673522afda50d6e00021d66bdc94d7aab6
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-20 01:31:54 -0700

    print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,8 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	if (i = 0; i < (n - 1); i++)
+	
+	for ((i = 0); i < (n - 1); i++)
 		printf("%d, ", a[i]);
 		if (i == (n - 1))
 			printf("%d", a[n - 1]);
