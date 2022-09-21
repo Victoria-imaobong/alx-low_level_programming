64dca2830f53883840d7a90734208ffcbc6e31ba
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-20 23:55:40 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 int main(void)
