3383cbc9cd5a4b910df24f095edbeca459d39a0a
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-22 13:26:09 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
+	int i = 0;
+	int j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
 	{
+		i++;
 	}
 
-	for (n = 0; (dest[i + n] != *src++) != '\0'; n++)
+	while (src[j] != '\0')
 	{
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
-
+	dest[i] = '\0';
 	return (dest);
 }
