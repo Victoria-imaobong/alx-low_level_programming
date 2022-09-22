248558c81add506548a897fc957e9b660f995930
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-09-22 12:48:50 -0700

    strncp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,15 @@
  * Return: returns dest
  */
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
+	        while (i < n)
+			dest[i++] = '\0';
+	
 	return (dest);
 }
