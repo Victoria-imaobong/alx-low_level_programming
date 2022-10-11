b5f56cc2ea55b036ee0ec95f558ccff5be80fa78
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-10-11 07:24:53 -0700

    mallocs

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - a function that allocates memory 
+ * @b: input amount
+ * Return: pointer to new memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == null)
+		exit(98);
+	return (n);
+}
