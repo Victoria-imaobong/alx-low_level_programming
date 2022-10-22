5709138322030a719e288bee2aaf26299090afe0
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-10-22 09:41:06 -0700

    add node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,21 @@
+#include "lists.h"
+/**
+ * list_len -  a function that returns the number
+ * of elements in a linked list_t list
+ * @h: struct
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
 
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
