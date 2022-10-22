1da16d87352f850a8d95376a375dfa7b8a1b76e1
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-10-22 08:52:39 -0700

    singly linked lists

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+A project onn when and why using linked lists vs arrays.
+How to build and use linked lists.
--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,1 +1,13 @@
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to be printed
+ *
+ * Return: 1 on success
+ * -1 on failure
+ */
+int _putchar(char c)
+{
+	return(write(1, &, 1));
+}
