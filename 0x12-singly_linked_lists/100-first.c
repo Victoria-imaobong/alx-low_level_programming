8404dbc1ce0d0f4b88b7967964518de622a976d0
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-10-22 10:38:18 -0700

    singly linked

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,11 @@
+#include "lists.h"
 
+/**
+ * bmain - function executed before main
+ * Return: 0
+ */
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * add_node - a function that adds a new node
  * at the beginning of a list_t list.
