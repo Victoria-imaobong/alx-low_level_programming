8b8b43cbd044780577da89129c6c8f0fe36ecaad
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-10-24 04:50:19 -0700

    more singly linkedlists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+/**
+ * listint_len -  function that returns the number of elements
+ * in a linked listint_t list.
+ * @h: head
+ * Return: 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
 
+	if (h == NULL)
+		return (0);
+
+	while (h != 0)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,25 @@
+#include "lists.h"
+/**
+ * add_nodeint - function that adds a new node
+ * at the beginning of a listint_t list.
+ * @head: head
+ * @n: value
+ * Return: 0
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
