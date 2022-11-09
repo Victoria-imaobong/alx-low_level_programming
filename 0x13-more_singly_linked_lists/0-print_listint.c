7b88a7de2f6e1b8dd41b7b60bb270ee58d0f2f30
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-11-09 06:46:14 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,6 +12,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
+	if (!h)
+		return (0);
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,9 +10,7 @@ size_t listint_len(const listint_t *h)
 	int count = 0;
 
 	if (h == NULL)
-		return (0);
-	if (!h)
-		return (0);
+		return (0);;
 
 	while (h != 0)
 	{
