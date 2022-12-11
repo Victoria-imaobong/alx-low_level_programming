4922f57db5193f29e35e7925f1a40b73bc00f5a5
Victoria-imaobong <solomonvictoria2002@gmail.com>
2022-12-11 23:41:57 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,4 +31,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-endif /* LISTS_H */
+#endif /* LISTS_H */
